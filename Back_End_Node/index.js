a800035d07034abe8a7c1dc771a8bc98c0cd634e
Gabriel Fernella Garcia dos Santos <GabrielFernella@users.noreply.github.com>
2020-03-24 12:58:37 -0300

    First Configuration BackEnd
    primeiras configurações do backend
    Rotas e pacotes, configurados e instalados respectivamente.

---
+++ Back_End_Node/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+/*
+    Métodos HTTP: 
+        Get - buscar info
+        Post - criar info 
+        Put - alterar info
+        Delete - deletar info
+*/
+/* 
+    Tipos de parametros
+        Query: parametros nomeados na rota após "?" para filtros e paginação
+        Route Params: parametros para identificar recursos
+        Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+*/
+app.post('/users', (req, res) => {
+    return res.json({
+        "evento": "Semana Omnistack 11"
+    })
+})
+
+app.listen(3333);
