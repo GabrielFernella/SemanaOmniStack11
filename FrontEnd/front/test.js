96915a60f28d5150e480eaa3c3a54e90e9717dca
Gabriel Fernella Garcia dos Santos <GabrielFernella@users.noreply.github.com>
2020-03-26 14:53:27 -0300

    Page Profile and CSS

---
+++ FrontEnd/front/test.js
@@ -0,0 +1,24 @@
+/*Olá pessoal tudo bem ? Estamos com vagas presenciais início imediato para os níveis Estágio(pelo CIEE), trainee, junior e Pleno para região oeste de SP. 
+
+Enviar CV e github para selecao@3xsolutions.com.br com o cargo desejado.
+
+Responsabilidades
+Desenvolvimento e suporte em aplicações Mobile/Web utilizando React Native/ReactJs .
+
+Buscamos um(a) profissional que seja antenado as novas tecnologias do mercado, ame <codar/>, aprender e compartilhar conhecimento! 
+
+Conhecimentos Necessários
+-    ReactJs/React Native
+- Boa estruturação de código
+-    Git 
+-    Conceitos sobre CI/CD
+- Familiaridade com Redux
+- Experiência em publicação de projetos
+- Conhecimento Scrum
+
+Legal se conhecer
+- VueJs
+- Noções de programação backend
+- Conhecimentos na linguagem NodeJs ou C#.*/
+
+
